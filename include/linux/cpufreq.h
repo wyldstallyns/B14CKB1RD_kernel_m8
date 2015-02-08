acbd3c1ddac8962f33b4eb89ca50369480f91826
JTrefzger <odd.ball.trefzger@gmail.com>
2015-02-08 02:05:32 -0500

    Revert "cpufreq: Add interface to set/get min/max freq 2"
    This reverts commit 526d539777105fc074b227a9c8e853176674ed14.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -287,10 +287,6 @@ int cpufreq_get_policy(struct cpufreq_policy *policy, unsigned int cpu);
 int cpufreq_update_policy(unsigned int cpu);
 bool have_governor_per_policy(void);
 int cpufreq_set_gov(char *target_gov, unsigned int cpu);
-int cpufreq_set_freq(unsigned int max_freq, unsigned int min_freq,
-			unsigned int cpu);
-int cpufreq_get_max(unsigned int cpu);
-int cpufreq_get_min(unsigned int cpu)
 
 #ifdef CONFIG_CPU_FREQ
 unsigned int cpufreq_get(unsigned int cpu);
