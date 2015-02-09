5c069c771b0b5051d10aa72ce0d51e21a32a7753
JTrefzger <jentrefzger@gmail.com>
2015-02-09 00:38:50 -0500

    ARM: gic: remove redundant debug message
    https://github.com/B14CKB1RD-Kernel/B14CKB1RD_Kernel_G2/commit/b99be55142533930042b9a5f9e67e58948f5cffd

--- arch/arm/common/gic.c
+++ arch/arm/common/gic.c
@@ -260,8 +260,6 @@ static void gic_show_resume_irq(struct gic_chip_data *gic)
 	for (i = find_first_bit(pending, gic->max_irq);
 	     i < gic->max_irq;
 	     i = find_next_bit(pending, gic->max_irq, i+1)) {
-		pr_warning("%s: %d triggered", __func__,
-					i + gic->irq_offset);
 		log_wakeup_reason(i + gic->irq_offset);
 #ifdef CONFIG_HTC_POWER_DEBUG
                 if (EE0_KRAIT_HLOS_SPMI_PERIPH_IRQ != i + gic->irq_offset)
