9a3f8ce9b6ae367943023be3d33005118ead043f
JTrefzger <jentrefzger@gmail.com>
2015-02-09 00:38:11 -0500

    POWER: Add an API call to log wake wakeup reason
    https://github.com/B14CKB1RD-Kernel/B14CKB1RD_Kernel_G2/commit/3dd8cfd779223c7a3e7e7e07b5423973d821110c

--- arch/arm/common/gic.c
+++ arch/arm/common/gic.c
@@ -39,6 +39,7 @@
 #include <linux/percpu.h>
 #include <linux/slab.h>
 #include <linux/syscore_ops.h>
+#include <linux/wakeup_reason.h>
 
 #include <asm/irq.h>
 #include <asm/exception.h>
@@ -261,6 +262,7 @@ static void gic_show_resume_irq(struct gic_chip_data *gic)
 	     i = find_next_bit(pending, gic->max_irq, i+1)) {
 		pr_warning("%s: %d triggered", __func__,
 					i + gic->irq_offset);
+		log_wakeup_reason(i + gic->irq_offset);
 #ifdef CONFIG_HTC_POWER_DEBUG
                 if (EE0_KRAIT_HLOS_SPMI_PERIPH_IRQ != i + gic->irq_offset)
                         if (TLMM_MSM_SUMMARY_IRQ != i + gic->irq_offset)
