1aa1db7647ddf50089366173f1c03b08681ae4d2
varchild <sirstreak@gmail.com>
2015-08-18 09:09:51 -0500

    Doesn't work right. :(
    Revert "cpufreq: create a helper function to get CPU utilization"
    
    This reverts commit 4d69a2542ba68e1bf3c9a3b7d878c7a03c4baa90.

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -1347,26 +1347,6 @@ static struct syscore_ops cpufreq_syscore_ops = {
 	.resume		= cpufreq_bp_resume,
 };
 
-/**
- * cpufreq_quick_get_util - get the CPU utilization from policy->util
- * @cpu: CPU number
- *
- * This is the last known util, without actually getting it from the driver.
- * Return value will be same as what is shown in util in sysfs.
- */
-unsigned int cpufreq_quick_get_util(unsigned int cpu)
-{
-	struct cpufreq_policy *policy = cpufreq_cpu_get(cpu);
-	unsigned int ret_util = 0;
-
-	if (policy) {
-		ret_util = policy->util;
-		cpufreq_cpu_put(policy);
-	}
-
-	return ret_util;
-}
-EXPORT_SYMBOL(cpufreq_quick_get_util);
 
 
 int cpufreq_register_notifier(struct notifier_block *nb, unsigned int list)
