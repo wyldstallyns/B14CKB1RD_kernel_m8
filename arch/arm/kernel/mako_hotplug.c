295efb157598c0442705cc7d7de8cfd30b0129db
varchild <sirstreak@gmail.com>
2015-08-18 10:00:48 -0500

    Doesn't work right. :( Revert "mako_hotplug: we can now cap max freq to 1GHz on screen off."
    This reverts commit ff906b1dca1bd3e5087821eee5393f468035140f.

--- arch/arm/kernel/mako_hotplug.c
+++ arch/arm/kernel/mako_hotplug.c
@@ -41,7 +41,6 @@
 #define MIN_CPU_UP_US 1000 * USEC_PER_MSEC;
 #define NUM_POSSIBLE_CPUS num_possible_cpus()
 #define HIGH_LOAD 90 * 2
-#define MAX_FREQ_CAP 1036800
 
 struct cpu_stats
 {
@@ -260,25 +259,6 @@ reschedule:
 		msecs_to_jiffies(t->timer * HZ));
 }
 
-static void screen_off_cap(bool nerf)
-{
-	int cpu;
-	uint32_t freq;
-	struct cpufreq_policy policy;
-
-	freq = nerf ? MAX_FREQ_CAP : LONG_MAX;	
-
-	for_each_online_cpu(cpu) {
-		cpufreq_get_policy(&policy, cpu);
-
-		cpufreq_verify_within_limits(&policy,
-			policy.cpuinfo.min_freq,
-			freq);
-
-		pr_info("CPU%d -> %d\n", cpu, policy.max);
-	}
-}
-
 static void mako_hotplug_suspend(struct work_struct *work)
 {
 	int cpu;
@@ -294,8 +274,6 @@ static void mako_hotplug_suspend(struct work_struct *work)
 	}
 
 	stats.online_cpus = num_online_cpus();
-
-	screen_off_cap(true);
 
 	pr_info("%s: suspend\n", MAKO_HOTPLUG);
 }
@@ -313,8 +291,6 @@ static void __ref mako_hotplug_resume(struct work_struct *work)
 	}
 
 	stats.online_cpus = num_online_cpus();
-
-	screen_off_cap(false);
 
 	pr_info("%s: resume\n", MAKO_HOTPLUG);
 }
